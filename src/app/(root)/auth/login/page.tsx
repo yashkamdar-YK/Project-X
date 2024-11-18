f117146a8e45d9cc8e966e7adaae071913763fd8
Yash Kamdar <kamdaryash7999@gmail.com>
2024-11-18 17:37:34 +0530

    Code Push

---
+++ src/app/(root)/auth/login/page.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { toast, useToast } from "@/hooks/use-toast";
+import Link from "next/link";
+import { useMutation } from "@tanstack/react-query";
+
+interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+const page = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+    const loginMutation = useMutation({
+      mutationFn: async (credentials: LoginCredentials) => {
+        const response = await login(credentials.email, credentials.password);
+        return response.data;
+      },
+      onSuccess: (data) => {
+        // Store necessary data in sessionStorage
+        sessionStorage.setItem("otpToken", data.otpToken);
+        sessionStorage.setItem("email", email);
+        sessionStorage.setItem("password", password);
+        router.push("/auth/verify-otp");
+      },
+      onError: (error) => {
+        console.log("error", error);
+        toast({
+          title: "Login Failed",
+          description:
+            error instanceof Error
+              ? (error as any).response?.data?.message ?? "Login failed"
+              : "An unexpected error occurred.",
+          variant: "destructive",
+        });
+      },
+    });
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    loginMutation.mutate({ email, password });
+  };
+  return (
+    <div>
+      <Card className="mx-auto max-w-sm">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl font-bold">Login</CardTitle>
+          <CardDescription>
+            Enter your email and password to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <div className="space-y-4">
+              <div className="space-y-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="space-y-2 ">
+                <Label htmlFor="password">Password</Label>
+                <Link
+                  href="/auth/forgot-password"
+                  className="text-sm ml-32 underline"
+                >
+                  Forgot your password?
+                </Link>
+                <Input
+                  id="password"
+                  type="password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+            </div>
+          </form>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="/auth/register" className="underline">
+              Register
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/providers.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { SessionProvider } from "next-auth/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { Toaster } from "./ui/toaster";
+import { ThemeProvider } from "./theme-provider";
+import { usePathname } from "next/navigation";
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  // Create a client
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+
+  const pathname = usePathname();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Toaster />
+          {children}
+        </ThemeProvider>
+      </SessionProvider>
+    </QueryClientProvider>
+  );
+};
