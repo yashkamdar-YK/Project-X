0e99adb53d18dc78f8ebc3889983152f4d3186e2
ankitpanchaal <ankit0panchal@gmail.com>
2024-12-18 14:33:43 +0530

    fix: rhs values

--- src/app/(root)/dashboard/strategy-builder/_components/StrategyNavbar/NodeSheet/ConditionNodeSheet/ConditionSubSection.tsx
+++ src/app/(root)/dashboard/strategy-builder/_components/StrategyNavbar/NodeSheet/ConditionNodeSheet/ConditionSubSection.tsx
@@ -1,18 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Plus, Trash2 } from 'lucide-react';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Plus, Trash2 } from "lucide-react";
 import { SubSection } from "./types";
-import { DataPoint } from '../../../DashboardSidebar/DatapointDialog/types';
-import { ALLOWED_OPERATIONS, DEFAULT_OPTIONS, VALID_DAYS } from './_const';
-import { useIndicatorStore } from '@/lib/store/IndicatorStore';
-import { useApplyDataStore } from '@/lib/store/applyDataStore';
-import { useMutation } from '@tanstack/react-query';
-import { useDataPointStore } from '@/lib/store/dataPointStore';
-import { defaultOptionsService } from '../../../../_actions';
-import { convertToMinutes } from '@/lib/utils';
-import Spinner from '@/components/shared/spinner';
+import { DataPoint } from "../../../DashboardSidebar/DatapointDialog/types";
+import { ALLOWED_OPERATIONS, DEFAULT_OPTIONS, VALID_DAYS } from "./_const";
+import { useIndicatorStore } from "@/lib/store/IndicatorStore";
+import { useApplyDataStore } from "@/lib/store/applyDataStore";
+import { useDataPointStore } from "@/lib/store/dataPointStore";
 
 interface ConditionSubSectionProps {
   subSection: SubSection;
@@ -46,57 +48,110 @@ export const ConditionSubSection: React.FC<ConditionSubSectionProps> = ({
   const { getData } = useApplyDataStore();
   const { selectedTimeFrame } = useDataPointStore();
 
-  const lhsOptions = [...dataPoints.map((dataPoint) => dataPoint.elementName),
-  ...indicators.map((indicator) => indicator.elementName),
-  ...DEFAULT_OPTIONS];
-  const selectedDatapoint = dataPoints.find((dataPoint) => dataPoint.elementName === subSection.lhs);
-  const selectedIndicator = indicators.find((indicator) => indicator.elementName === subSection.lhs);
-  const columns = selectedDatapoint?.options?.columnsAvailable || [];
-  const hasCandleLocation = selectedDatapoint?.options?.candleLocation || false;
-
-  //@ts-ignore
-  const allowedOperations = ALLOWED_OPERATIONS[selectedDatapoint?.options?.type]?.length > 0
-    //@ts-ignore
-    ? ALLOWED_OPERATIONS[selectedDatapoint?.options?.type]
-    //@ts-ignore
-    : ALLOWED_OPERATIONS[selectedIndicator?.options?.type]?.length > 0
-      //@ts-ignore
-      ? ALLOWED_OPERATIONS[selectedIndicator?.options?.type]
-      //@ts-ignore
-      : ALLOWED_OPERATIONS[getData(subSection.lhs)?.type]?.length > 0
-        //@ts-ignore
-        ? ALLOWED_OPERATIONS[getData(subSection.lhs)?.type]
-        //@ts-ignore
-        : ALLOWED_OPERATIONS[subSection?.lhs];
-
-  const _canCompareWith = selectedDatapoint?.options?.canComparedwith?.map(v => v.toLocaleLowerCase()) || [];
-
-  const canCompareWith = [
-    //@ts-ignore
-    ...dataPoints?.filter(v => _canCompareWith.includes(v.options?.type.toLocaleLowerCase())).map(v => v.elementName),
-    //@ts-ignore
-    ...indicators?.filter(v => _canCompareWith.includes(v.options?.type.toLocaleLowerCase())).map(v => v.elementName),
-    ...(_canCompareWith.includes("values") ? ["value"] : []),
-    ...DEFAULT_OPTIONS
+  const lhsOptions = [
+    ...dataPoints.map((dataPoint) => dataPoint.elementName),
+    ...indicators.map((indicator) => indicator.elementName),
+    ...DEFAULT_OPTIONS,
   ];
 
-  useEffect(() => {
-    if (subSection.lhs === "day_of_week") {
-      updateSubSection(nodeId, subSection.id, "rhs", "Monday");
+  // const selectedDatapoint = dataPoints.find((dataPoint) => dataPoint.elementName === subSection.lhs);
+  // const selectedIndicator = indicators.find((indicator) => indicator.elementName === subSection.lhs);
+
+  const selectedLHS = () => {
+    return (
+      dataPoints.find(
+        (dataPoint) => dataPoint.elementName === subSection.lhs
+      ) ||
+      indicators.find(
+        (indicator) => indicator.elementName === subSection.lhs
+      ) ||
+      subSection.lhs
+    );
+  };
+
+  //@ts-ignore
+  const columns = selectedLHS()?.options?.columnsAvailable || [];
+  //@ts-ignore
+  const hasCandleLocation = selectedLHS()?.options?.candleLocation || false;
+
+  const getOperatorType = () => {
+    //@ts-ignore
+    if (selectedLHS()?.options?.type) {
+      //@ts-ignore
+      return selectedLHS().options.type;
     }
-  }, [subSection.lhs]);
+    //@ts-ignore
+    if (getData(subSection.lhs)?.type) {
+      //@ts-ignore
+      return getData(subSection.lhs).type;
+    }
+    return subSection.lhs;
+  };
+
+  const operatorType = getOperatorType();
+  //@ts-ignore
+  const allowedOperations = ALLOWED_OPERATIONS[operatorType] || [];
+
+  //@ts-ignore
+  const _canCompareWith = selectedLHS()?.options
+    ? //@ts-ignore
+      selectedLHS()?.options?.canComparedwith?.map((v) => v.toLocaleLowerCase())
+    : //@ts-ignore
+      getData(subSection.lhs)?.canComparedwith?.map((v) =>
+        v.toLocaleLowerCase()
+      );
+
+  const getRHSOptions = useCallback(() => {
+    //@ts-ignore
+    const _DEFAULT_OPTIONS = [];
+    DEFAULT_OPTIONS.forEach((v) => {
+      //@ts-ignore
+      const options = getData(v);
+      //@ts-ignore
+      const type = options?.type;
+      if (type && _canCompareWith?.includes(type.toLocaleLowerCase())) {
+        _DEFAULT_OPTIONS.push(v);
+      }
+    });
+    const data = [
+      ...dataPoints
+        ?.filter((v) =>
+          _canCompareWith?.includes(v.options?.type.toLocaleLowerCase())
+        )
+        .map((v) => v.elementName),
+      //@ts-ignore
+      ...indicators
+        ?.filter((v) =>
+          _canCompareWith?.includes(v.options?.type.toLocaleLowerCase())
+        )
+        .map((v) => v.elementName),
+      ...(_canCompareWith?.includes("values") ? ["value"] : []),
+      //@ts-ignore
+      ..._DEFAULT_OPTIONS,
+    ];
+
+    if (
+      subSection.operator === "cross_above" ||
+      subSection.operator === "cross_below"
+    ) {
+      return data.filter((v) => v !== subSection.lhs);
+    }
+
+    return data;
+  }, [_canCompareWith]);
 
   // useEffect(() => {
-  //   if (subSection.lhs === "candle_time") {
-  //     //@ts-ignore
-  //     updateSubSection(nodeId, subSection.id, "rhs", `${getData("OpenTime")[0]}`);
+  //   if (subSection.lhs === "day_of_week") {
+  //     updateSubSection(nodeId, subSection.id, "rhs", "Monday");
   //   }
-  //   if (subSection.lhs === "candle_close_time") {
-  //     //@ts-ignore
-  //     updateSubSection(nodeId, subSection.id, "rhs", `${getData("CloseTime")[1]}`);
-  //   }
-  //     //@ts-ignore
-  // }, [selectedTimeFrame]);
+  // }, [subSection.lhs]);
+
+  const renderOperatorLabel = (operator: any) => {
+    if (typeof operator?.label === "function") {
+      return operator?.label();
+    }
+    return operator?.label;
+  };
 
   return (
     <div className="mb-6 last:mb-0 pb-6 border-b border-gray-700 last:border-b-0">
@@ -106,14 +161,24 @@ export const ConditionSubSection: React.FC<ConditionSubSectionProps> = ({
           <div className="flex -space-x-[1px]">
             <Select
               value={subSection.lhs}
-              onValueChange={(value) => updateSubSection(nodeId, subSection.id, "lhs", value)}
+              onValueChange={(value) =>
+                updateSubSection(nodeId, subSection.id, "lhs", value)
+              }
             >
-              <SelectTrigger className={`w-fit border-r-0 ${!columns.length && !hasCandleLocation ? 'rounded-r-md' : 'rounded-r-none'}`}>
+              <SelectTrigger
+                className={`w-fit border-r-0 ${
+                  !columns.length && !hasCandleLocation
+                    ? "rounded-r-md"
+                    : "rounded-r-none"
+                }`}
+              >
                 <SelectValue placeholder="Select variable" />
               </SelectTrigger>
               <SelectContent>
                 {lhsOptions.map((option) => (
-                  <SelectItem key={option} value={option}>{option}</SelectItem>
+                  <SelectItem key={option} value={option}>
+                    {option}
+                  </SelectItem>
                 ))}
               </SelectContent>
             </Select>
@@ -121,14 +186,23 @@ export const ConditionSubSection: React.FC<ConditionSubSectionProps> = ({
             {columns.length > 0 && (
               <Select
                 value={subSection.column}
-                onValueChange={(value) => updateSubSection(nodeId, subSection.id, "column", value)}
+                onValueChange={(value) =>
+                  updateSubSection(nodeId, subSection.id, "column", value)
+                }
               >
-                <SelectTrigger className={`w-fit rounded-none border-x-0 ${!hasCandleLocation ? 'rounded-r-md' : ''}`}>
+                <SelectTrigger
+                  className={`w-fit rounded-none border-x-0 ${
+                    !hasCandleLocation ? "rounded-r-md" : ""
+                  }`}
+                >
                   <SelectValue placeholder="Select column" />
                 </SelectTrigger>
                 <SelectContent>
+                  {/* @ts-ignore */}
                   {columns.map((option) => (
-                    <SelectItem key={option} value={option}>{option}</SelectItem>
+                    <SelectItem key={option} value={option}>
+                      {option}
+                    </SelectItem>
                   ))}
                 </SelectContent>
               </Select>
@@ -138,9 +212,22 @@ export const ConditionSubSection: React.FC<ConditionSubSectionProps> = ({
               <div className="flex -space-x-[1px]">
                 <Select
                   value={subSection.selectedPeriod}
-                  onValueChange={(value) => updateSubSection(nodeId, subSection.id, "selectedPeriod", value)}
+                  onValueChange={(value) =>
+                    updateSubSection(
+                      nodeId,
+                      subSection.id,
+                      "selectedPeriod",
+                      value
+                    )
+                  }
                 >
-                  <SelectTrigger className={`w-fit border-x-0 ${subSection.selectedPeriod !== "prev-n" ? 'rounded-r-md' : 'rounded-none'}`}>
+                  <SelectTrigger
+                    className={`w-fit border-x-0 ${
+                      subSection.selectedPeriod !== "prev-n"
+                        ? "rounded-r-md"
+                        : "rounded-none"
+                    }`}
+                  >
                     <SelectValue placeholder="Select period" />
                   </SelectTrigger>
                   <SelectContent>
@@ -154,7 +241,14 @@ export const ConditionSubSection: React.FC<ConditionSubSectionProps> = ({
                   <Input
                     type="number"
                     value={subSection.nValue}
-                    onChange={(e) => updateSubSection(nodeId, subSection.id, "nValue", e.target.value)}
+                    onChange={(e) =>
+                      updateSubSection(
+                        nodeId,
+                        subSection.id,
+                        "nValue",
+                        e.target.value
+                      )
+                    }
                     placeholder="n (1-20)"
                     className="w-20 rounded-l-none"
                     min="1"
@@ -169,14 +263,27 @@ export const ConditionSubSection: React.FC<ConditionSubSectionProps> = ({
         {/* Operator */}
         <Select
           value={subSection.operator}
-          onValueChange={(value) => updateSubSection(nodeId, subSection.id, "operator", value)}
+          onValueChange={(value) =>
+            updateSubSection(nodeId, subSection.id, "operator", value)
+          }
         >
           <SelectTrigger className="w-[150px]">
-            <SelectValue placeholder="Select operator" />
+            <SelectValue placeholder="Select operator">
+              {subSection.operator &&
+                renderOperatorLabel(
+                  allowedOperations.find(
+                    //@ts-ignore
+                    (op) => op.value === subSection.operator
+                  )
+                )}
+            </SelectValue>
           </SelectTrigger>
           <SelectContent>
-            {allowedOperations?.map((option: string) => (
-              <SelectItem key={option} value={option}>{option}</SelectItem>
+            {/* @ts-ignore */}
+            {allowedOperations.map((operation) => (
+              <SelectItem key={operation.value} value={operation.value}>
+                {renderOperatorLabel(operation)}
+              </SelectItem>
             ))}
           </SelectContent>
         </Select>
@@ -186,42 +293,63 @@ export const ConditionSubSection: React.FC<ConditionSubSectionProps> = ({
           <div className="flex -space-x-[1px]">
             <Select
               value={subSection.rhs}
-              onValueChange={(value) => updateSubSection(nodeId, subSection.id, "rhs", value)}
+              onValueChange={(value) =>
+                updateSubSection(nodeId, subSection.id, "rhs", value)
+              }
             >
-              <SelectTrigger className={`w-fit ${subSection.rhs === "value" ? 'rounded-r-none border-r-0' : ''}`}>
+              <SelectTrigger
+                className={`w-fit ${
+                  subSection.rhs === "value" ? "rounded-r-none border-r-0" : ""
+                }`}
+              >
                 <SelectValue placeholder="Select variable" />
               </SelectTrigger>
               <SelectContent>
-                {
-                  subSection.lhs === "candle_close_time"
-                    //@ts-ignore
-                    ? getData("CloseTime")?.map((option) => (
-                      <SelectItem key={option} value={option}>{option}</SelectItem>
-                    )) :
-                    subSection.lhs === "candle_time"
-                      //@ts-ignore
-                      ? getData("OpenTime")?.map((option) => (
-                        <SelectItem key={option} value={option}>{option}</SelectItem>
-                      ))
-                      : subSection.lhs == "day_of_week"
-                        ? VALID_DAYS.map((option) => (
-                          <SelectItem key={option} value={option}>{option}</SelectItem>
-                        ))
-                        : canCompareWith.map((option) => (
-                          <SelectItem key={option} value={option}>{option}</SelectItem>
-                        ))}
+                {subSection.lhs === "candle_close_time"
+                  ? //@ts-ignore
+                    getData("CloseTime")?.map((option) => (
+                      <SelectItem key={option} value={option}>
+                        {option}
+                      </SelectItem>
+                    ))
+                  : subSection.lhs === "candle_time"
+                  ? //@ts-ignore
+                    getData("OpenTime")?.map((option) => (
+                      <SelectItem key={option} value={option}>
+                        {option}
+                      </SelectItem>
+                    ))
+                  : subSection.lhs === "day_of_week"
+                  ? VALID_DAYS.map((option) => (
+                      <SelectItem key={option} value={option}>
+                        {option}
+                      </SelectItem>
+                    ))
+                  : getRHSOptions().map((option) => (
+                      <SelectItem key={option} value={option}>
+                        {option}
+                      </SelectItem>
+                    ))}
               </SelectContent>
             </Select>
 
-            {subSection.rhs === "value" && (
-              <Input
-                type="text"
-                value={subSection._rhsValue}
-                onChange={(e) => updateSubSection(nodeId, subSection.id, "_rhsValue", e.target.value)}
-                placeholder="Enter value"
-                className="w-[120px] rounded-l-none"
-              />
-            )}
+            {subSection.rhs === "value" &&
+              getRHSOptions()?.includes("value") && (
+                <Input
+                  type="text"
+                  value={subSection._rhsValue}
+                  onChange={(e) =>
+                    updateSubSection(
+                      nodeId,
+                      subSection.id,
+                      "_rhsValue",
+                      e.target.value
+                    )
+                  }
+                  placeholder="Enter value"
+                  className="w-[120px] rounded-l-none"
+                />
+              )}
           </div>
         </div>
 
---
+++ src/app/(root)/dashboard/strategy-builder/_components/StrategyNavbar/NodeSheet/ConditionNodeSheet/_const.tsx
@@ -0,0 +1,205 @@
+export const ALLOWED_OPERATIONS = {
+  candleData: [
+    {
+      label: ">",
+      value: "is_above",
+    },
+    {
+      label: "<",
+      value: "is_below",
+    },
+    {
+      label: ">=",
+      value: "above_or_equal",
+    },
+    {
+      label: "<=",
+      value: "below_or_equal",
+    },
+    {
+      label: "=",
+      value: "equals",
+    },
+    {
+      label: () => <CrossBelowIcon />,
+      value: "cross_below",
+    },
+    {
+      label: () => <CrossAboveIcon />,
+      value: "cross_above",
+    },
+  ],
+  indicators:[
+    {
+      label: ">",
+      value: "is_above",
+    },
+    {
+      label: "<",
+      value: "is_below",
+    },
+    {
+      label: ">=",
+      value: "above_or_equal",
+    },
+    {
+      label: "<=",
+      value: "below_or_equal",
+    },
+    {
+      label: "=",
+      value: "equals",
+    },
+    {
+      label: () => <CrossBelowIcon />,
+      value: "cross_below",
+    },
+    {
+      label: () => <CrossAboveIcon />,
+      value: "cross_above",
+    },
+  ],
+  candleTime:[
+    {
+      label: ">",
+      value: "is_above",
+    },
+    {
+      label: "<",
+      value: "is_below",
+    },
+    {
+      label: ">=",
+      value: "above_or_equal",
+    },
+    {
+      label: "<=",
+      value: "below_or_equal",
+    },
+    {
+      label: "=",
+      value: "equals",
+    },
+  ],
+  dte:[
+    {
+      label: ">",
+      value: "is_above",
+    },
+    {
+      label: "<",
+      value: "is_below",
+    },
+    {
+      label: ">=",
+      value: "above_or_equal",
+    },
+    {
+      label: "<=",
+      value: "below_or_equal",
+    },
+    {
+      label: "=",
+      value: "equals",
+    },
+  ],
+  day_of_week:[
+    {
+      label: "=",
+      value: "equals",
+    },
+  ],
+  mtm:[
+    {
+      label: ">",
+      value: "is_above",
+    },
+    {
+      label: "<",
+      value: "is_below",
+    },
+    {
+      label: ">=",
+      value: "above_or_equal",
+    },
+    {
+      label: "<=",
+      value: "below_or_equal",
+    },
+    {
+      label: "=",
+      value: "equals",
+    },
+  ]
+};
+
+export const VALID_DAYS = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+export const DEFAULT_OPTIONS = [
+  "day_mtm",
+  "mtm_from_first_open_pos",
+  "open_mtm",
+  "candle_time",
+  "candle_close_time",
+  "day_of_week",
+];
+const CrossBelowIcon = () => (
+  <svg
+    viewBox="0 0 43 40"
+    className="w-6 h-6 dark:stroke-white stroke-gray-800"
+  >
+    <path
+      d="M26.6831 25.2162L16.637 15.1702"
+      className="stroke-gray-800 dark:stroke-white"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+    <path
+      d="M40.7238 20.4299L2.5964 19.4749"
+      className="stroke-gray-300 dark:stroke-gray-600"
+      strokeWidth="4"
+      strokeLinecap="round"
+      strokeDasharray="6 6"
+    />
+    <path
+      d="M30.8529 29.641L30.853 20.2209L21.4329 29.641H30.8529Z"
+      className="fill-gray-800 stroke-gray-800 dark:fill-white dark:stroke-white"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+  </svg>
+);
+
+const CrossAboveIcon = () => (
+  <svg
+    viewBox="0 0 43 39"
+    className="w-6 h-6 dark:stroke-white stroke-gray-800"
+  >
+    <path
+      d="M13.2621 24.6819L23.3081 14.6358"
+      className="stroke-gray-800 dark:stroke-white"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+    <path
+      d="M40.7238 20.1364L2.5964 19.1814"
+      className="stroke-gray-300 dark:stroke-gray-600"
+      strokeWidth="4"
+      strokeLinecap="round"
+      strokeDasharray="6 6"
+    />
+    <path
+      d="M29.0382 8.94885L19.6181 8.94879L29.0382 18.3689V8.94885Z"
+      className="fill-gray-800 stroke-gray-800 dark:fill-white dark:stroke-white"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+  </svg>
+);
